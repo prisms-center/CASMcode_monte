108b564afdeb8b98ca749ef221c676fcf9c98a88
bpuchala <bpuchala@users.noreply.github.com>
2021-10-28 12:09:01 -0400

    add forward declarations to definitions.hh

--- include/casm/monte/definitions.hh
+++ include/casm/monte/definitions.hh
@@ -16,6 +16,38 @@ typedef double TimeType;
 /// Map of value name to vector value
 typedef std::map<std::string, Eigen::VectorXd> VectorValueMap;
 
+struct CompletionCheckParams;
+struct SamplingParams;
+
+template <typename _ConfigType, typename _RunInfoType>
+class ConfigGenerator;
+template <typename _ConfigType>
+class FixedConfigGenerator;
+
+template <typename _ConfigType>
+struct Results;
+
+template <typename _ConfigType>
+class ResultsIO;
+
+template <typename _ConfigType>
+struct State;
+
+template <typename _ConfigType, typename _RunInfoType>
+class StateGenerator;
+template <typename _ConfigType>
+class IncrementalConditionsStateGenerator;
+
+template <typename _ConfigType>
+class StateSampler;
+
+template <typename _ConfigType>
+struct StateSamplingFunction;
+
+template <typename _ConfigType>
+using StateSamplingFunctionMap =
+    std::map<std::string, StateSamplingFunction<_ConfigType>>;
+
 }  // namespace monte
 }  // namespace CASM
 
